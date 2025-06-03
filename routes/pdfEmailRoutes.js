d85933e27c4d40674724b84f58085a0d60817e8d
vichitra rajan <vichi100@gmail.com>
2025-06-03 22:29:47 +0530

    agent id updated

--- routes/pdfEmailRoutes.js
+++ routes/pdfEmailRoutes.js
@@ -4,7 +4,11 @@ const { generatePdf } = require('../controllers/pdfController');
 
 router.post('/generate', async (req, res) => {
     try {
-        const agent_id = "17434188543883987866347";//req.params.propertyId;
+        const { req_user_id } = req.body; // Extract agent_id from the request body
+        if (!req_user_id) {
+            return res.status(400).send({ error: 'agent_id is required in the request body' });
+        }
+        const agent_id =req_user_id;// //req.params.propertyId;
         const pdfPath = await generatePdf(agent_id);
         res.status(200).send({ message: 'PDF generated successfully', pdfPath });
     } catch (error) {
